8879b53eecf5f58170575b5bf1b47d04d8256dc5
caio-k <koiti2000@hotmail.com>
2020-09-12 20:34:46 -0300

    removed code regarding the moderator profile

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Register from "./components/register.component";
 import Home from "./components/home.component";
 import Profile from "./components/profile.component";
 import BoardUser from "./components/board-user.component";
-import BoardModerator from "./components/board-moderator.component";
 import BoardAdmin from "./components/board-admin.component";
 
 class App extends Component {
@@ -31,7 +30,6 @@ class App extends Component {
     if (user) {
       this.setState({
         currentUser: user,
-        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
         showAdminBoard: user.roles.includes("ROLE_ADMIN")
       });
     }
@@ -42,7 +40,7 @@ class App extends Component {
   }
 
   render() {
-    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+    const { currentUser, showAdminBoard } = this.state;
 
     return (
         <Router>
@@ -57,14 +55,6 @@ class App extends Component {
                     Home
                   </Link>
                 </li>
-
-                {showModeratorBoard && (
-                    <li className="nav-item">
-                      <Link to={"/mod"} className="nav-link">
-                        Moderator Board
-                      </Link>
-                    </li>
-                )}
 
                 {showAdminBoard && (
                     <li className="nav-item">
@@ -120,7 +110,6 @@ class App extends Component {
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/profile" component={Profile} />
                 <Route path="/user" component={BoardUser} />
-                <Route path="/mod" component={BoardModerator} />
                 <Route path="/admin" component={BoardAdmin} />
               </Switch>
             </div>
