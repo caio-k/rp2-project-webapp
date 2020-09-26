32dab0ae5f8ebcee712aabb8d24b8319a58fb0ec
caio-k <koiti2000@hotmail.com>
2020-09-26 14:46:14 -0300

    changed server base URL

--- src/api/server-api.js
+++ src/api/server-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import authHeader from '../services/auth-header';
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://safe-school-server.herokuapp.com",
   responseType: "json",
   headers: authHeader(),
 });
